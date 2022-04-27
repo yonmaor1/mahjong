51ae2c5bf43b4215a780f2b797eb8a39b0cc02bb
yonmaor1 <yonmaor@cmu.edu>
2022-04-27 15:11:35 -0400

    discarded changes

--- scratchpad.py
+++ scratchpad.py
@@ -4,18 +4,30 @@ from displays import *
 from mahjong import *
 pygame.init()
 
-screen = pygame.display.set_mode((WIDTH + SIDEBAR, HEIGHT))
+def mouseTime():
+    screen = pygame.display.set_mode((WIDTH + SIDEBAR, HEIGHT))
+    screen.fill(GREEN)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                startTime = time.time()
+            if event.type == pygame.MOUSEBUTTONUP:
+                endTime = time.time()
+                return endTime - startTime
+
+
+'''screen = pygame.display.set_mode((WIDTH + SIDEBAR, HEIGHT))
 deck = getDeck()
 players = initPlayers(deck)
 giveHands(players, deck)
 turn = 0
 players[0].name = 'yon'
 players[0].won = True
-endGame(players, turn, screen)
+endGame(players, turn, screen)'''
 
 
-'''
-deck = getDeck()
+
+'''deck = getDeck()
 for i in range(len(deck) - 1):
     for j in range(i+1, len(deck)):
         if deck[i] is deck[j]:
